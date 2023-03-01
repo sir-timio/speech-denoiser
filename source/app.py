95a4a6100e6cc36bca24f9298c42d778ab6ed6c6
sir-timio <t-ionov@list.ru>
2023-03-01 23:52:55 +0300

    fine

--- source/app.py
+++ source/app.py
@@ -2,14 +2,21 @@ from distutils.log import debug
 import os
 import uuid
 from flask import Flask, flash, request, redirect, send_file, jsonify
-from base64 import b64decode
+
+# model 
+from IPython import display as disp
+import torch
+import torchaudio
+from denoiser import pretrained
+from denoiser.dsp import convert_audio
 import librosa
+    
 
 UPLOAD_FOLDER = 'files'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key = '123'
+app.secret_key = '321'
 
 @app.route('/')
 def root():
@@ -23,22 +30,36 @@ def save_record():
     if not audio_data:
         return jsonify({'success': False, 'message': 'No record found'}), 400
     
-    fname = os.path.join(app.config['UPLOAD_FOLDER'], str(uuid.uuid4()) + ".wav")
+    uid = str(uuid.uuid4())
+    fname = os.path.join(app.config['UPLOAD_FOLDER'], uid + ".wav")
     audio_data.save(fname)
     
+    file = fname
+    
+    assert MODEL is None, 'load model first'
+    wav, sr = librosa.load(file)
+    wav = convert_audio(torch.tensor(wav[None,:]), sr, MODEL.sample_rate, MODEL.chin)
+    with torch.no_grad():
+        denoised = MODEL(wav[None])[0]
+    
+    # denoised_fname = 
+    
+    return send_file()
+        
     return jsonify({'success': True, 'message': 'Record saved'}), 200
 
 
-@app.route('/play-audio')
-def play_audio():
-    audio_file = '/Users/timur/web-denoiser/files/1d9c88ea-0768-4362-bc43-e63ebbf4fad9.mp3'
-    if os.path.isfile(audio_file):
-        return send_file(audio_file, mimetype='audio/wav')
-    else:
-        return 'Audio file not found'
+@app.route('/denoised_audio/<file>')
+def denoise(file: str) -> str:
+    assert MODEL is None, 'load model first'
+    wav, sr = librosa.load(file)
+    wav = convert_audio(torch.tensor(wav[None,:]), sr, MODEL.sample_rate, MODEL.chin)
+    with torch.no_grad():
+        denoised = MODEL(wav[None])[0]
+    
+    return denoised
 
-def denoise(model, audiofile):
-    pass
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    MODEL = pretrained.dns64().cpu()
+    app.run(debug=False)
