f55c51aa8c5762f08730b29bfca2eed9eabad573
sir-timio <t-ionov@list.ru>
2023-03-01 16:31:20 +0300

    init

---
+++ source/app.py
@@ -0,0 +1,38 @@
+import os
+import uuid
+from flask import Flask, flash, request, redirect
+
+UPLOAD_FOLDER = 'files'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = '123'
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+
+@app.route('/save-record', methods=['POST'])
+def save_record():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    file_name = str(uuid.uuid4()) + ".mp3"
+    full_file_name = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+    file.save(full_file_name)
+    return '<h1>Success</h1>'
+
+
+def denoise(model, audiofile):
+    pass
+
+if __name__ == '__main__':
+    app.run()
