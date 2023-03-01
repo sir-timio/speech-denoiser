3044ea8d6f2bddaf33a16b4b2abf1242a22105a0
sir-timio <t-ionov@list.ru>
2023-03-01 17:25:17 +0300

    done with uploading

--- source/app.py
+++ source/app.py
@@ -1,6 +1,9 @@
+from distutils.log import debug
 import os
 import uuid
-from flask import Flask, flash, request, redirect
+from flask import Flask, flash, request, redirect, send_file, jsonify
+from base64 import b64decode
+import librosa
 
 UPLOAD_FOLDER = 'files'
 
@@ -14,25 +17,28 @@ def root():
 
 
 @app.route('/save-record', methods=['POST'])
-def save_record():
-    # check if the post request has the file part
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    # if user does not select file, browser also
-    # submit an empty part without filename
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
-    file_name = str(uuid.uuid4()) + ".mp3"
-    full_file_name = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
-    file.save(full_file_name)
-    return '<h1>Success</h1>'
+def save_record():    
+    audio_data = request.files.get('data')
+    
+    if not audio_data:
+        return jsonify({'success': False, 'message': 'No record found'}), 400
+    
+    fname = os.path.join(app.config['UPLOAD_FOLDER'], str(uuid.uuid4()) + ".wav")
+    audio_data.save(fname)
+    
+    return jsonify({'success': True, 'message': 'Record saved'}), 200
 
+
+@app.route('/play-audio')
+def play_audio():
+    audio_file = '/Users/timur/web-denoiser/files/1d9c88ea-0768-4362-bc43-e63ebbf4fad9.mp3'
+    if os.path.isfile(audio_file):
+        return send_file(audio_file, mimetype='audio/wav')
+    else:
+        return 'Audio file not found'
 
 def denoise(model, audiofile):
     pass
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
