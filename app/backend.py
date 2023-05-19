99fc4fbd78759517ff3699a6f93f2a6df4438bb4
sir-timio <t-ionov@list.ru>
2023-05-19 23:57:54 +0300

    working bot

--- app/backend.py
+++ app/backend.py
@@ -3,38 +3,67 @@ import os
 import sys
 
 sys.path.append("../app")
+import base64
+import io
 import uuid
 from os.path import join as join_path
 
 import librosa
 import numpy as np
+import requests
 import soundfile as sf
 import torchaudio
 import uvicorn
 from audio_recorder_streamlit import audio_recorder
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, Response, UploadFile
 from matplotlib import pyplot as plt
+from pydub import AudioSegment
 
 from src.engine import cold_run, denoise, transcribe
 from src.models import load_denoiser, load_transcriber
 from src.utils import load_config, parse_args, plot_wave
 
+
+def ogg_to_wav(file) -> bytes:
+    """Telegram отправляет голосовые сообщения в формате ogg
+    с кодеком OPUS. soundfile поддерживает ogg,
+    но только с кодеком Vorbis, а не OPUS."""
+    audio = AudioSegment.from_ogg(file)
+    byte_io = io.BytesIO()
+    audio.export(byte_io, format="wav")
+    byte_io.seek(0)
+    return byte_io
+
+
+def np_to_ogg(waveform, sr):
+    # convert numpy array to audio file
+    audio = AudioSegment(
+        waveform.tobytes(),
+        frame_rate=sr,
+        sample_width=2,
+        channels=1,
+    )
+
+    byte_io = io.BytesIO()
+    audio.export(byte_io, format="ogg")
+    byte_io.seek(0)
+    return byte_io
+
+
 app = FastAPI()
 
 
-@app.get("/process_audio")
+@app.post("/process_audio")
 async def process_audio(file: UploadFile = File(...)):
-    contents = await file.read()
-    waveform, sr = sf.read(file=io.BytesIO(contents), dtype="float32")
-    byte_io = io.BytesIO()
-    sf.write(byte_io, waveform, sr, format="WAV")
-    audio_bytes = byte_io.getvalue()
-    return audio_bytes
+    converted_audio = ogg_to_wav(io.BytesIO(await file.read()))
+    waveform, sr = sf.read(converted_audio, dtype="int16")
 
-
-@app.get("/process_filename")
-async def process_filename(file: UploadFile = File(...)):
-    return 200
+    # do some
+    audio_bytes = np_to_ogg(waveform, sr)
+    return {
+        "voice": base64.b64encode(audio_bytes.getvalue()).decode("utf-8"),
+        "text": "",
+    }
 
 
 if __name__ == "__main__":
--- app/bot.py
+++ app/bot.py
@@ -2,15 +2,17 @@ import sys
 
 sys.path.append("../app")
 sys.path.append("..")  # for docker
-from io import BytesIO
+import base64
+import io
 
+import aiohttp
 import librosa
 import requests
 import soundfile as sf
 import yaml
 from addict import Dict
 from PIL import Image
-from telegram import Update, Voice
+from telegram import InputFile, Update
 from telegram.ext import Application, ContextTypes, MessageHandler, filters
 
 from src.utils import load_config, parse_args
@@ -20,17 +22,34 @@ async def handle_voice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     """audio handler"""
     voice = await update.message.voice.get_file()
 
-    # voice_load_response = requests.get(photo.file_path)
-    # image_bytes = BytesIO(image_load_response.content)
+    # Download the voice file
+    async with aiohttp.ClientSession() as session:
+        async with session.get(voice.file_path) as resp:
+            voice_bytes = await resp.read()
 
-    response = requests.get(
-        "http://ml.n19:30025/process_audio_url",
-        files={"file": voice.file_id},
-    )
-
-    await update.message.reply_text(
-        str(response.status_code), reply_to_message_id=update.message.id
-    )
+    # Send the file to the server
+    async with aiohttp.ClientSession() as session:
+        data = aiohttp.FormData()
+        data.add_field("file", io.BytesIO(voice_bytes), filename="voice.ogg")
+        async with session.post(
+            "http://localhost:30025/process_audio", data=data
+        ) as resp:
+            if resp.status == 200:
+                body = await resp.json()
+                denoised_voice = InputFile(
+                    io.BytesIO(base64.b64decode(body["voice"])),
+                    filename="denoised_voice.ogg",
+                )
+                await update.message.reply_voice(
+                    denoised_voice,
+                    caption=f"{str(resp.status)}: {body['text']}",
+                    reply_to_message_id=update.message.id,
+                )
+            else:
+                await update.message.reply_text(
+                    f"{resp.status}: {resp.reason}",
+                    reply_to_message_id=update.message.id,
+                )
 
 
 def main() -> None:
